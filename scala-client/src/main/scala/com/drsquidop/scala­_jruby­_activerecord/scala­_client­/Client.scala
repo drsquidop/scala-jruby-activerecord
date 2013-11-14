873729cc2e2db6dbe7de318ea5dd433678ea2d1c
Dao Ngoc <ngoc@Daos-MacBook-Pro.local>
2013-11-15 00:34:00 +0900

    ??? is cooler than null

--- scala-client/src/main/scala/com/drsquidop/scala­_jruby­_activerecord/scala­_client­/Client.scala
+++ scala-client/src/main/scala/com/drsquidop/scala­_jruby­_activerecord/scala­_client­/Client.scala
@@ -3,9 +3,9 @@ package com.drsquidop.scala­_jruby­_activerecord.scala_client
 import scala.util.Try
 
 object Client {
-  def create(myModel: MyModel): Try[MyModel] = null
+  def create(myModel: MyModel): Try[MyModel] = ???
   
-  def get(id: Int): Try[MyModel] = null
+  def get(id: Int): Try[MyModel] = ???
   
-  def all(): Try[Seq[MyModel]] = null
+  def all(): Try[Seq[MyModel]] = ???
 }
